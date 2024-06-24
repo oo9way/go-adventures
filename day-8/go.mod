bbf66a6e2ce6da5264f918815b0a062ee370fb81
Oogway <mohammedyousef.dev@gmail.com>
2024-06-24 14:00:00 +0100

    Day-8

---
+++ day-8/go.mod
@@ -0,0 +1,5 @@
+module first-project
+
+go 1.18
+
+require github.com/softchris/math v0.2.0
---
+++ day-8/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"first-project/helper"
+	"fmt"
+	"github.com/softchris/math"
+)
+
+func main() {
+	helper.Help()
+	sum := math.Add(1, 2)
+	fmt.Println(sum)
+
+}
